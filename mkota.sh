6d7d678201132944a666c24a9bffd16535fad993
nift4 <36458596+nift4@users.noreply.github.com>
2020-05-14 11:06:48 +0200

    Add ota creator

---
+++ mkota.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo {
+echo 	\"datetime\": `cat $2 | cut -d "=" --output-delimiter "," -f 1,2 | awk -F, -v findex=1 -v value=post-timestamp '$findex == value {print}' | cut -d, -f 2`,
+echo 	\"filename\": \"$1\",
+echo 	\"id\": \"`sha256sum $1 | awk '{ print $1 }'`\",
+echo 	\"romtype\": \"unoffical\",
+echo 	\"size\": `stat -c%s $1`,
+echo 	\"url\": undefined,
+echo 	\"version\": \"17.1\"
+echo }
