048b03ce0f1c5a0c7b0e7365b11372d68649bf61
nift4 <nift4@protonmail.com>
2020-05-14 16:44:05 +0200

    Fix

--- mkota-wrapper.sh
+++ mkota-wrapper.sh
@@ -1,3 +1,3 @@
-IFS=']' read -r -a json < ota/$2.json
+IFS=']' read -r -a json < ota/$3.json
 NEWDATA=`./mkota.sh $1 $2`
-echo ${json[1]}${NEWDATA}${json[2]}> ota/$2.json
+echo ${json[1]}${NEWDATA}${json[2]} > ota/$3.json
