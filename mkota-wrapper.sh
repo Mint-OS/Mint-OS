a8a66f4d101dc8ac871e268b6310656afc73ff10
nift4 <nift4@protonmail.com>
2020-05-14 20:58:41 +0200

    Fix nonsense

--- mkota-wrapper.sh
+++ mkota-wrapper.sh
@@ -1,5 +1,5 @@
 IFS=']' read -r -a json < ota/$3.json
 NEWDATA=`./mkota.sh $1 $2`
-echo ${json[1]}${NEWDATA}",]"${json[2]} > ota/$3.json
+echo ${json[1]}","${NEWDATA}"]"${json[2]} > ota/$3.json
 git add ota/$3.json
 git commit -m "Automatic OTA Update"
