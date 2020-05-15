f724009218caef6a60418853ed0d41288f230d4b
nift4 <nift4@protonmail.com>
2020-05-15 08:17:52 +0200

    Tweakidoodle

--- mkota.sh
+++ mkota.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-echo {
-echo 	\"datetime\": `cat $2 | cut -d "=" --output-delimiter "," -f 1,2 | awk -F, -v findex=1 -v value=post-timestamp '$findex == value {print}' | cut -d, -f 2`,
-echo 	\"filename\": \"$1\",
-echo 	\"id\": \"`sha256sum $1 | awk '{ print $1 }'`\",
-echo 	\"romtype\": \"unoffical\",
-echo 	\"size\": `stat -c%s $1`,
-echo 	\"url\": \"https://downloads.sourceforge.net/project/mint-os-project/$1\",
-echo 	\"version\": \"17.1\"
-echo }
+echo -n {\"datetime\": `cat $2 | cut -d "=" --output-delimiter "," -f 1,2 | awk -F, -v findex=1 -v value=post-timestamp '$findex == value {print}' | cut -d, -f 2`,\"filename\": \"$1\",\"id\": \"`sha256sum $1 | awk '{ print $1 }'`\",\"romtype\": \"unoffical\",\"size\": `stat -c%s $1`,\"url\": \"https://downloads.sourceforge.net/project/mint-os-project/$1\",\"version\": \"17.1\"}
