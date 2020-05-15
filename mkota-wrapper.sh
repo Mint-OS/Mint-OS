68669aadca193a8d8f5d9507b87a9c20e5f77b78
kiam001 <blablatupfen@gmail.com>
2020-05-15 07:31:38 +0200

    Add mint path

--- mkota-wrapper.sh
+++ mkota-wrapper.sh
@@ -1,6 +1,6 @@
 IFS=']' read -r -a json < ota/$3.json
-chmod +x mkota.sh
-NEWDATA=`./mkota.sh $1 $2`
-echo ${json[1]}","${NEWDATA}"]"${json[2]} > ota/$3.json
+chmod +x $4/mkota.sh
+NEWDATA=`$4/mkota.sh $1 $2`
+echo ${json[1]}","${NEWDATA}"]"${json[2]} > $4/ota/$3.json
 git add ota/$3.json
 git commit -m "$3: Automatic OTA Update"
