c143f8d625fa3f69d0cfe88567a27cbea8a3b220
nift4 <nift4@protonmail.com>
2020-05-15 07:43:41 +0200

    Continue Cleanup

--- mkota-wrapper.sh
+++ mkota-wrapper.sh
@@ -1,7 +1,6 @@
-cd $4
 IFS=']' read -r -a json < ota/$3.json
 chmod +x mkota.sh
 NEWDATA=`./mkota.sh $1 $2`
-echo ${json[1]}","${NEWDATA}"]"${json[2]} > ota/$3.json
+echo "${json[0]},${NEWDATA}]${json[1]}" > ota/$3.json
 git add ota/$3.json
 git commit -m "$3: Automatic OTA Update"
