143d024bfd42cc7df07c97d2960f23aed05ce6ff
nift4 <nift4@protonmail.com>
2020-07-13 21:08:04 +0200

    Revert "Fix ARCore by using different fingerprint"
    This reverts commit d1404d394cf4834b7ddeab511b6ebb5b881f4810.

--- device/cedric/device.mk
+++ device/cedric/device.mk
@@ -5,6 +5,5 @@ prepare:
 
 patch:
 	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cd $$MINTOS_DIR/kernel/motorola/msm8937 && wget -O- https://github.com/nift4/android-keyboard-gadget/raw/linux-3.18/generic_kernel_3.18.patch | patch -p1
-	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cd $$MINTOS_DIR/device/motorola/cedric && wget -O- https://github.com/Sohamlad7/android_device_motorola_cedric/commit/49e9ac7bdccc03729dff0a3f4eff676a75ef3d38 | patch -p1
+	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cd $$MINTOS_DIR/device/motorola/cedric && wget -O- https://github.com/Sohamlad7/android_device_motorola_cedric/commit/fe1564fb738ec698ade687a43be4e9e3b7270de2.patch | patch -p1
 	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cd $$MINTOS_DIR/device/motorola/cedric && wget -O- https://github.com/Sohamlad7/android_device_motorola_cedric/commit/851f1323a06cc08a2d77f73d2e2afc9ab2b26357.patch | patch -p1
-	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cd $$MINTOS_DIR/device/motorola/cedric && awk '{sub("grep_fingerprint_magic_lol","google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys")}1' init/init_cedric.cpp > temp.txt && mv temp.txt init/init_cedric.cpp
