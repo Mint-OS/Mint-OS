4bd9be5303c5d848fc2c23409a91dbf6773208f4
nift4 <nift4@protonmail.com>
2020-11-15 08:32:55 +0100

    Update mkota-wrapper.sh

--- mkota-wrapper.sh
+++ mkota-wrapper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-ls $1 || git commit -m "$3: Automatic OTA Update FAILED"
+ls $1 || (cd $5; git commit -m "$3: Automatic OTA Update FAILED"; cd -)
 ls $1 || exit 1 
-IFS=']' read -r -a json < ota/$3.json
+IFS=']' read -r -a json < $5/$3.json
 chmod +x mkota.sh
 NEWDATA=`./mkota.sh $1 $2 $4`
-echo "${json[0]},${NEWDATA}]${json[1]}" > ota/$3.json
-git add ota/$3.json
+echo "${json[0]},${NEWDATA}]${json[1]}" > $5/$3.json
+cd $5
+git add $3.json
 git commit -m "$3: Automatic OTA Update"
+cd -
