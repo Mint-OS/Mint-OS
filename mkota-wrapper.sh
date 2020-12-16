833560f7b0467ecd6c843591de24daf71d30f43c
nift4 <nift4@protonmail.com>
2020-12-16 11:33:06 +0100

    Create mkota-wrapper.sh

---
+++ mkota-wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ls $1 || exit 1
+IFS=']' read -r -a json < $3.json
+chmod +x mkota.sh
+NEWDATA=`./mkota.sh $1 $2 $4`
+echo "${json[0]},${NEWDATA}]${json[1]}" > $3.json
+git add $3.json
+git commit -m "$3: Automatic OTA Update"
+git push github HEAD:ota
