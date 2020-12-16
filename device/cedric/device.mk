74284b2a0ec81e552583e15aa064f17f5b468956
nift4 <nift4@protonmail.com>
2020-12-16 08:59:05 +0100

    Update device.mk

--- device/cedric/device.mk
+++ device/cedric/device.mk
@@ -2,6 +2,3 @@ all: prepare patch
 
 prepare:
 	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cp $$MINTOS_DAT/device/cedric/90-cedric.xml $$MINTOS_DIR/.repo/local_manifests
-
-patch:
-	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cd $$MINTOS_DIR/device/motorola/cedric && wget -O- https://github.com/moto-msm8937/android_device_motorola_cedric/commit/a7feb87698b684ae944190a9a7a9dfdd35854e7b.patch | patch -p1
