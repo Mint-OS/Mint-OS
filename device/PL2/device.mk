bd9c02a8dbdf784e8e79b4b4a1d0bbd1c02cd0d1
nift4 <nift4@protonmail.com>
2020-12-16 11:25:33 +0100

    Delete device.mk

--- device/PL2/device.mk
+++
@@ -1,4 +0,0 @@
-all: prepare
-
-prepare:
-	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cp $$MINTOS_DAT/device/PL2/90-PL2.xml $$MINTOS_DIR/.repo/local_manifests
