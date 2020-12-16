010a1f904555590b1ff28b9b063f7ddd5130dad8
nift4 <nift4@protonmail.com>
2020-12-16 08:59:13 +0100

    Update device.mk

--- device/cedric/device.mk
+++ device/cedric/device.mk
@@ -1,4 +1,4 @@
-all: prepare patch
+all: prepare
 
 prepare:
 	@export MINTOS_DIR=`cat .makedata` MINTOS_DAT=`pwd` MINTOS_DEVICE=`cat .makedata2` && cp $$MINTOS_DAT/device/cedric/90-cedric.xml $$MINTOS_DIR/.repo/local_manifests
