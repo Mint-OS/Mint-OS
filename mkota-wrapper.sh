c66a333b474e8dca3cef110c67ea612aa104ab74
nift4 <nift4@protonmail.com>
2020-12-16 11:30:06 +0100

    Update mkota-wrapper.sh

--- mkota-wrapper.sh
+++ mkota-wrapper.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-OTADIR=../MintOS_ota
-ls $1 || (cd $OTADIR; git commit -m "$3: Automatic OTA Update FAILED"; cd -)
 ls $1 || exit 1
-IFS=']' read -r -a json < $OTADIR/$3.json
+IFS=']' read -r -a json < $3.json
 chmod +x mkota.sh
 NEWDATA=`./mkota.sh $1 $2 $4`
-echo "${json[0]},${NEWDATA}]${json[1]}" > $OTADIR/$3.json
-cd $OTADIR
+echo "${json[0]},${NEWDATA}]${json[1]}" > $3.json
 git add $3.json
 git commit -m "$3: Automatic OTA Update"
-cd -
+git push github HEAD:ota
